2ef0f8dab429ec7f735e46e11d1c963af1d7fa66
izal <58200011+rzlahd@users.noreply.github.com>
2024-03-27 21:37:34 +0700

    Create datetime.php

---
+++ php/datetime.php
@@ -0,0 +1,10 @@
+echo "<br>".date("j F Y");
+// 27 March 2024
+echo "<br>".date("jS F Y");
+// 27th March 2024
+echo "<br>".date("jS \of F Y");
+// 27th of March 2024
+echo "<br>".date("jS \of F Yh:i:s A");
+// 27th of March 2024 02:35:53 PM
+echo date("l jS \of F Y h:i:s A") . "<br>";
+// Wednesday 27th of March 2024 02:36:48 PM
