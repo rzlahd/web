636997319fa02911ea1fd07d3dee013d36201f6d
izal <58200011+rzlahd@users.noreply.github.com>
2024-03-28 03:51:27 +0700

    Update ajax.php

--- javascript/ajax.php
+++ javascript/ajax.php
@@ -1,1 +1,18 @@
-
+<script>
+$.ajax({
+  accepts: {
+    mycustomtype: 'application/x-some-custom-type'
+  },
+ 
+  // Instructions for how to deserialize a `mycustomtype`
+  converters: {
+    'text mycustomtype': function(result) {
+      // Do Stuff
+      return newresult;
+    }
+  },
+ 
+  // Expect a `mycustomtype` back from server
+  dataType: 'mycustomtype'
+});
+</script>
